a0c402d4c9626e922ddd9ec6cbacfba698dc86e5
Maryna M <markova.maryna@gmail.com>
2022-11-29 22:19:09 +0200

    add API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,30 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather defaultCity="Drohobych" />
+        <footer>
+          <a
+            href="https://github.com/MarynaMarkova/React-Weather-App"
+            className="profileLink"
+            target="_blank"
+            rel="noreferrer"
+            title="Github code"
+          >
+            Open-source
+          </a>
+          {" by "}
+          <a
+            href="https://marynamarkova.github.io/"
+            className="profileLink"
+            target="_blank"
+            rel="noreferrer"
+            title="Personal page"
+          >
+            Maryna Markova
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,109 +1,110 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  let weatherData = {
-    city: "Drohobych",
-    feels: 15,
-    humidity: 38,
-    wind: 7,
-    temperature: 18,
-    description: "cloudy",
-    day: "Monday",
-    date: 23,
-    month: "November",
-    hours: 22,
-    minutes: 48,
-    imageUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-  };
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [temperature, setTemperature] = useState(null);
+  function handleResponse(response) {
+    console.log(response.data);
+    console.log(response.data.temperature.current);
 
-  return (
-    <div className="Weather">
-      <div className="row heading">
-        <div className="col-3">
-          <ul className="details">
-            <li>
-              Feels like: <span className="feels">{weatherData.feels}</span>ºC
-            </li>
-            <li>
-              Humidity: <span className="humidity">{weatherData.humidity}</span>
-              %
-            </li>
-            <li>
-              Wind: <span className="wind">{weatherData.wind}</span> km/h
-            </li>
-          </ul>
-        </div>
+    setWeatherData({
+      ready: true,
+      city: response.data.city,
+      feels: response.data.temperature.feels_like,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      temperature: response.data.temperature.current,
+      description: response.data.condition.description,
+      day: "Monday",
+      date: 23,
+      month: "November",
+      hours: 22,
+      minutes: 48,
+      imageUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    });
+  }
 
-        <div className="col-6">
-          <h1 className="cityPlace">{weatherData.city}</h1>
-          <h2>
-            <img
-              src={weatherData.imageUrl}
-              className="weather-icon-big"
-              alt={weatherData.description}
-            />
-            <span className="temperature-big">{weatherData.temperature}</span>
-            <span className="units-big">ºC</span>
-          </h2>
-          <h3 className="description">{weatherData.description}</h3>
-        </div>
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="row heading">
+          <div className="col-3">
+            <ul className="details">
+              <li>
+                Feels like:{" "}
+                <span className="feels">{Math.round(weatherData.feels)}</span>ºC
+              </li>
+              <li>
+                Humidity:{" "}
+                <span className="humidity">{weatherData.humidity}</span>%
+              </li>
+              <li>
+                Wind:{" "}
+                <span className="wind">{Math.round(weatherData.wind)}</span>{" "}
+                km/h
+              </li>
+            </ul>
+          </div>
 
-        <div className="col-3">
-          <form className="search-forn">
-            <div className="mb-3">
-              <input
-                type="search"
-                className="form-control search-block cityInput"
-                placeholder="Enter a city"
-                autoFocus="on"
+          <div className="col-6">
+            <h1 className="cityPlace">{weatherData.city}</h1>
+            <h2>
+              <img
+                src={weatherData.imageUrl}
+                className="weather-icon-big"
+                alt={weatherData.description}
               />
+              <span className="temperature-big">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="units-big">ºC</span>
+            </h2>
+            <h3 className="description">{weatherData.description}</h3>
+          </div>
 
-              <input type="submit" className="submit" value="Go" />
-            </div>
-          </form>
+          <div className="col-3">
+            <form className="search-forn">
+              <div className="mb-3">
+                <input
+                  type="search"
+                  className="form-control search-block cityInput"
+                  placeholder="Enter a city"
+                  autoFocus="on"
+                />
 
-          <button type="button" className="btn btn-success currentLocation">
-            Current location
-          </button>
+                <input type="submit" className="submit" value="Go" />
+              </div>
+            </form>
 
-          <br />
+            <button type="button" className="btn btn-success currentLocation">
+              Current location
+            </button>
 
-          <ul className="fullDate">
-            <li className="day">{weatherData.day}</li>
-            <li>
-              <span className="date">{weatherData.date}</span> of{" "}
-              <span className="month">{weatherData.month}</span>
-            </li>
-            <li>
-              <span className="hours">{weatherData.hours}</span>:
-              <span className="minutes">{weatherData.minutes}</span>
-            </li>
-          </ul>
+            <br />
+
+            <ul className="fullDate">
+              <li className="day">{weatherData.day}</li>
+              <li>
+                <span className="date">{weatherData.date}</span> of{" "}
+                <span className="month">{weatherData.month}</span>
+              </li>
+              <li>
+                <span className="hours">{weatherData.hours}</span>:
+                <span className="minutes">{weatherData.minutes}</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div className="author">
-        <a
-          href="https://github.com/MarynaMarkova/React-Weather-App"
-          className="profileLink"
-          target="_blank"
-          rel="noreferrer"
-          title="Github code"
-        >
-          Open-source
-        </a>
-        {" by "}
-        <a
-          href="https://www.linkedin.com/in/maryna-markova/"
-          className="profileLink"
-          target="_blank"
-          rel="noreferrer"
-          title="Linkedin"
-        >
-          Maryna Markova
-        </a>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = `7fc8ceb1c3d0dcbob733fd665t774f9a`;
+    let units = `metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
