75c51cb08d7b246fd87b542ecabe42ecb9f1132b
Maryna M <markova.maryna@gmail.com>
2022-12-27 16:11:58 +0200

    remove the unused variable

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
-  let unit = "celsius";
   return (
     <div className="weatherTemperature">
       <span className="temperature-big">{Math.round(props.celsius)}</span>
